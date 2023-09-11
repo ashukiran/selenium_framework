702c58a81fb1575119ee118ce9eafe725d569e1f
ashwini rao <ashwini rao@Ashukiran>
2023-09-11 12:21:58 +0530

    1st commit

---
+++ doopeProject/src/test/java/doopeProject/Dummy_test.java
@@ -0,0 +1,15 @@
+package doopeProject;
+
+import org.testng.annotations.Test;
+
+@Test
+public class Dummy_test {
+	
+	public void method1() {
+		System.out.println("i am learning automation");
+	}
+	
+public void method2() {
+	System.out.println("i am working on automation framework");
+}
+}
---
+++ doopeProject/src/test/java/doopeProject/dummy_class1.java
@@ -0,0 +1,16 @@
+package doopeProject;
+
+import org.testng.annotations.Test;
+import com.google.common.annotations.VisibleForTesting;
+
+@Test
+public class dummy_class1 {
+
+	public void m1() {
+		System.out.println("hii,how r u!!!");
+	}
+public void m2() {
+	System.out.println("I am fine");
+}
+
+}
