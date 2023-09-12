aaf76957688af2832321c26fc9c8d032c2203207
ashukiran <ashwinihr15@gmail.com>
2023-09-12 17:42:26 +0530

    new push

---
+++ doopeProject/src/test/java/doopeProject/Dummy_test.java
@@ -0,0 +1,18 @@
+package doopeProject;
+
+import org.testng.annotations.Test;
+
+@Test
+public class Dummy_test {
+	
+	public void method1() {
+		System.out.println("i am learning automation");
+	}
+	
+public void method2() {
+	System.out.println("i am working on automation framework");
+}
+}
+//push ashwini-git bash
+//1st pull using GitBash
+//pulling back from github
